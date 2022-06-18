b28ed6e16033044bd44a49a1df6d8549e95ba644
Simbeomsu <82105396+Simbeomsu@users.noreply.github.com>
2022-06-18 19:13:11 +0900

    Delete flask directory

--- flask/flask02.py
+++
@@ -1,9 +0,0 @@
-from flask08_파일업로드 import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-	return render_template('input.html')
-
-if __name__ == "__main__":
-		app.run()
